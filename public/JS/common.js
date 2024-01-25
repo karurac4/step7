524c5e4bffa1878c7d71211dcbfaba5d7fad7646
karurac4 <ckarura4@gmail.com>
2024-01-25 09:24:13 +0900

    step8 correction2

---
+++ public/JS/common.js
@@ -0,0 +1,68 @@
+
+$(document).ready(function () {
+    $('.btn-danger').on('click', function (e) {
+        e.preventDefault();
+
+        var productId = $(this).data('product-id');
+        console.log('Product ID:', productId);
+
+        if (confirm('本当に削除しますか？')) {
+            $.ajax({
+                type: 'POST',
+                url: `/products/${productId}`,
+                data: {
+                    _token: $('meta[name="csrf-token"]').attr('content'),
+                    _method: 'DELETE'
+                    },
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                success: function (data) {
+                    console.log('Ajax request succeeded:', data);
+
+                    if (data.success) {
+                        $(`#productRow${productId}`).hide();
+                        alert('商品を削除しました');
+
+                        // ページの再読み込み
+                        location.reload();
+                    } else {
+                        alert('削除できませんでした');
+                    }
+                },
+                error: function () {
+                    console.error('Ajax request failed');
+                    alert('削除できませんでした');
+                }
+            });
+        }
+    });
+
+    $(".tablesorter").tablesorter();
+});
+
+    
+
+    
+    
+  $(document).ready(function() {
+    $('#searchButton').on('click', function() {
+        $('tbody').empty();
+        $('.pagination').empty();
+
+        $.ajax({
+            url: productsIndexUrl,
+            type: "GET",
+            data: $('#searchForm').serialize(),
+            success: function(data) {
+                $('table:not(#searchResults table)').hide();
+                $('#searchResults').html(data);
+            },
+            error: function(error) {
+                console.error('Ajax request failed', error);
+            }
+        });
+    });
+});
+
+
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -118,78 +118,11 @@
     
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.3/js/jquery.tablesorter.min.js"></script>
-
-<script>
-$(document).ready(function () {
-    $('.btn-danger').on('click', function (e) {
-        e.preventDefault();
-
-        var productId = $(this).data('product-id');
-        console.log('Product ID:', productId);
-
-        if (confirm('本当に削除しますか？')) {
-            $.ajax({
-                type: 'POST',
-                url: `/products/${productId}`,
-                data: {
-                    _token: $('meta[name="csrf-token"]').attr('content'),
-                    _method: 'DELETE'
-                    },
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    },
-                success: function (data) {
-                    console.log('Ajax request succeeded:', data);
-
-                    if (data.success) {
-                        $(`#productRow${productId}`).hide();
-                        alert('商品を削除しました');
-
-                        // ページの再読み込み
-                        location.reload();
-                    } else {
-                        alert('削除できませんでした');
-                    }
-                },
-                error: function () {
-                    console.error('Ajax request failed');
-                    alert('削除できませんでした');
-                }
-            });
-        }
-    });
-
-    $(".tablesorter").tablesorter();
-});
-</script>
-
-    
-
-    
-    
     <script>
-  $(document).ready(function() {
-    $('#searchButton').on('click', function() {
-        $('tbody').empty();
-        $('.pagination').empty();
-
-        $.ajax({
-            url: "{{ route('products.index') }}",
-            type: "GET",
-            data: $('#searchForm').serialize(),
-            success: function(data) {
-                $('table:not(#searchResults table)').hide();
-                $('#searchResults').html(data);
-            },
-            error: function(error) {
-                console.error('Ajax request failed', error);
-            }
-        });
-    });
-});
-
-
+    const productsIndexUrl = "{{ route('products.index') }}";
     </script>
+    <script src="{{ asset('js/common.js') }}"></script>
+
 
     
 @endsection
